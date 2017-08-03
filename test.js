81e964faad3bb8da5f3b4b920f332d1b220300a4
Miles Johnson <mileswjohnson@gmail.com>
2017-08-03 00:24:35 -0700

    Regenerate version datasets.

--- test.js
+++ test.js
@@ -1,32 +1,13 @@
-// const loadNames = require('./lib/loaders/loadNames').default;
-// const loadData = require('./lib/loaders/loadData').default;
-// const loadSequences = require('./lib/loaders/loadSequences').default;
-// const loadZwjSequences = require('./lib/loaders/loadZwjSequences').default;
-// const loadVariations = require('./lib/loaders/loadVariations').default;
-// const loadOrderAndGroup = require('./lib/loaders/loadOrderAndGroup').default;
-//
-// loadNames();
-// loadOrderAndGroup();
-// loadVariations();
-//
-// for (let i = 1; i <= 5; i += 1) {
-//   loadData(`${i}.0`);
-//   loadSequences(`${i}.0`);
-//   loadZwjSequences(`${i}.0`);
-// }
-
 const generateData = require('./lib/generators/generateData').default;
 const generateVersions = require('./lib/generators/generateVersions').default;
 
 process.on('unhandledRejection', (error) => {
-  console.error('unhandledRejection', error.stack);
+  console.error(error.stack);
 });
 
-Promise.all([
-  generateData(),
-  // generateVersions(),
-])
-  .then()
+Promise.resolve()
+  .then(() => generateVersions())
+  .then(() => generateData())
   .catch((error) => {
     console.error(error);
   });
