272480a64d32d9498a99437dbd4c098ca7be0bab
Miles Johnson <mileswjohnson@gmail.com>
2017-08-05 00:29:47 -0700

    Added flattenEmojiData.

--- src/flattenEmojiData.js
+++ src/flattenEmojiData.js
@@ -7,5 +7,22 @@
 export default function flattenEmojiData(data: Object[]): Object[] {
   const emojis = [];
 
+  data.forEach((emoji) => {
+    if (emoji.skins) {
+      const { skins, ...restEmoji } = emoji;
+
+      emojis.push(restEmoji);
+
+      skins.forEach((skin) => {
+        emojis.push({
+          ...skin,
+          tags: emoji.tags,
+        });
+      });
+    } else {
+      emojis.push(emoji);
+    }
+  });
+
   return emojis;
 }
