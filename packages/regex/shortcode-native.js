96b327e1791a9b1735b3dc00c1f32ed88e74a6bc
Miles Johnson <mileswjohnson@gmail.com>
2020-08-14 17:29:25 -0700

    fix: Allow plus signs `+` in shortcodes.

--- packages/regex/shortcode-native.js
+++ packages/regex/shortcode-native.js
@@ -3,15 +3,15 @@
 // http://www.fileformat.info/info/unicode/block/index.htm
 const ranges = [
   // Chinese
-  /[\d_\u4E00-\u9FFF-]/,
+  /[\d+_\u4E00-\u9FFF-]/,
   // Japanese
-  /[\d_\u3000-\u30FF-]/,
+  /[\d+_\u3000-\u30FF-]/,
   // Korean
-  /[\d_\u1100-\u11FF\u3130-\u318F\uA960-\uA97F\uAC00-\uD7FF-]/,
+  /[\d+_\u1100-\u11FF\u3130-\u318F\uA960-\uA97F\uAC00-\uD7FF-]/,
   // Thai
-  /[\d_\u0E00-\u0E7F-]/,
+  /[\d+_\u0E00-\u0E7F-]/,
   // Russian, Ukrainian
-  /[\d_a-z\u0400-\u052F\u1C80-\u1C8F\u2DE0-\u2DFF\uA640-\uA69F-]/,
+  /[\d+_a-z\u0400-\u052F\u1C80-\u1C8F\u2DE0-\u2DFF\uA640-\uA69F-]/,
 ];
 
 module.exports = new RegExp(`:(${ranges.map((range) => range.source).join('|')})+:`, 'i');
