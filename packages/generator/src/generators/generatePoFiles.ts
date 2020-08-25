ba6e5ab7178242c394f501e7eb088e63c1ecb802
Miles Johnson <mileswjohnson@gmail.com>
2020-08-25 16:19:47 -0700

    Add custom translations.

--- packages/generator/src/generators/generatePoFiles.ts
+++ packages/generator/src/generators/generatePoFiles.ts
@@ -3,6 +3,12 @@ import { ShortcodesDataset, SUPPORTED_LOCALES, Emoji, Hexcode, GroupDataset } fr
 import log from '../helpers/log';
 import toArray from '../helpers/toArray';
 import writeFile from '../helpers/writeFile';
+import {
+  REGIONAL_INDICATOR_MESSAGES,
+  SKIN_TONE_MESSAGES,
+  SYMBOL_ASTERISK_MESSAGES,
+  SYMBOL_HASH_MESSAGES,
+} from '../translations';
 
 export default async function generatePoFiles(): Promise<void> {
   log.title('data', 'Generating I18N po files');
@@ -89,7 +95,45 @@ export default async function generatePoFiles(): Promise<void> {
 
   await Promise.all(
     SUPPORTED_LOCALES.map(async (locale) => {
-      const output = locale === 'en' || locale === 'en-gb' ? englishOutput : nonEnglishOutput;
+      const output = [
+        ...(locale === 'en' || locale === 'en-gb' ? englishOutput : nonEnglishOutput),
+      ];
+
+      // Regional indicators
+      output.unshift(
+        '',
+        '# Custom',
+        '#, javascript-format',
+        `msgctxt "ANNOTATION: Denotes a countries flag using alphabetical characters (usa = United States)"`,
+        `msgid "regional indicator %s"`,
+        `msgstr "${REGIONAL_INDICATOR_MESSAGES[locale] || ''}"`,
+      );
+
+      // Skin tones
+      output.unshift(
+        '',
+        '# Custom',
+        `msgctxt "ANNOTATION: Very short word for skin tone and/or color (max 1 word if possible)"`,
+        `msgid "skin tone"`,
+        `msgstr "${SKIN_TONE_MESSAGES[locale] || ''}"`,
+      );
+
+      // Symbols
+      output.unshift(
+        '',
+        '# Custom',
+        `msgctxt "ANNOTATION: Name of the asterisk symbol (*)"`,
+        `msgid "asterisk"`,
+        `msgstr "${SYMBOL_ASTERISK_MESSAGES[locale] || ''}"`,
+      );
+
+      output.unshift(
+        '',
+        '# Custom',
+        `msgctxt "ANNOTATION: Name of the number sign/hash symbol (#)"`,
+        `msgid "number sign"`,
+        `msgstr "${SYMBOL_HASH_MESSAGES[locale] || ''}"`,
+      );
 
       return writeFile(path.join(process.cwd(), 'po'), `${locale}.po`, output.join('\n'));
     }),
