e1431ff5b219d7a50c253f0bf761e346d1fb4bab
Miles Johnson <mileswjohnson@gmail.com>
2020-10-15 20:27:33 -0700

    Run build.

--- packages/generator/src/loaders/loadEmoticons.ts
+++ packages/generator/src/loaders/loadEmoticons.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { EmoticonMap } from '../types';
 
 export default function loadEmoticons(): Promise<EmoticonMap> {
+  // Relative to the lib/ folder!
   // eslint-disable-next-line
-  return Promise.resolve(require(path.resolve(__dirname, '../resources/emoticons.js')).default);
+  return Promise.resolve(require(path.resolve(__dirname, 'emoticons.js')));
 }
