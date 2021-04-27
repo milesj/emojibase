f6ade80de48e1a743e550c77ae09cdbd49c5841f
Miles Johnson <mileswjohnson@gmail.com>
2021-04-27 14:16:01 -0700

    Update docs.

--- website/sidebars.js
+++ website/sidebars.js
@@ -18,8 +18,8 @@ module.exports = {
       items: [
         {
           type: 'link',
-          label: 'CLDR v38.1',
-          href: 'http://cldr.unicode.org/index/downloads/cldr-38',
+          label: 'CLDR v39',
+          href: 'http://cldr.unicode.org/index/downloads/cldr-39',
         },
         {
           type: 'link',
