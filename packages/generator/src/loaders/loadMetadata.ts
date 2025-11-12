27b93a1650bed3a0f51a6e260378f5908a30bd4b
Folf <folf@folf.me>
2025-11-12 07:41:05 +0100

    revert emoji-test.txt to official source

--- packages/generator/src/loaders/loadMetadata.ts
+++ packages/generator/src/loaders/loadMetadata.ts
@@ -8,8 +8,7 @@ export async function loadMetadata(
 ): Promise<EmojiMetadataMap> {
 	return fetchAndCache(
 		Number(version) >= 17
-		// TODO: emoji-test.txt for v17 is currently dead, change this when resolved.
-			? `https://distfiles.alpinelinux.org/distfiles/v3.23/emoji-test-17.0.0.txt` //`https://unicode.org/Public/${version}.0/emoji/emoji-test.txt`
+			? `https://unicode.org/Public/${version}.0/emoji/emoji-test.txt`
 			: `http://unicode.org/Public/emoji/${version}/emoji-test.txt`,
 		`${version}/metadata.json`,
 		parseMetadata,
